6d9d8e345302da189796ed0f750b194e0c818325
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-10-11 20:46:14 +0900

    Added stetho

--- app/src/main/java/tokyo/tommy_kw/kotlinsample/api/ApiClient.kt
+++ app/src/main/java/tokyo/tommy_kw/kotlinsample/api/ApiClient.kt
@@ -1,10 +1,13 @@
 package tokyo.tommy_kw.kotlinsample.api
 
+import com.facebook.stetho.okhttp.StethoInterceptor
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.google.gson.internal.bind.DateTypeAdapter
+import com.squareup.okhttp.OkHttpClient
 import retrofit.RestAdapter
 import retrofit.android.AndroidLog
+import retrofit.client.OkClient
 import retrofit.converter.GsonConverter
 import rx.Observable
 import rx.functions.Func1
@@ -24,11 +27,15 @@ class ApiClient {
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                 .registerTypeAdapter(Date::class.java, DateTypeAdapter()).create()
 
+        val client = OkHttpClient()
+        client.networkInterceptors().add(StethoInterceptor())
+
         mRestAdapter = RestAdapter.Builder()
                 .setEndpoint(ENDPOINT)
                 .setConverter(GsonConverter(gsonBuilder))
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .setLog(AndroidLog("=NETWORK="))
+                .setClient(OkClient(client))
                 .build();
 
         mApiService = mRestAdapter.create(ApiService::class.java)
--- app/src/main/java/tokyo/tommy_kw/kotlinsample/application/App.kt
+++ app/src/main/java/tokyo/tommy_kw/kotlinsample/application/App.kt
@@ -1,6 +1,8 @@
 package tokyo.tommy_kw.kotlinsample.application
 
 import android.app.Application
+import android.util.Log
+import com.facebook.stetho.Stetho
 
 /**
  * Created by tommy on 15/10/07.
@@ -12,5 +14,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Stetho.initializeWithDefaults(this);
     }
 }
