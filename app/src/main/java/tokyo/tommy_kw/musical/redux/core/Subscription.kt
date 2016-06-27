e0cfb52f04549a6326258c46d9c524e2f1765dba
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-06-27 12:24:44 +0900

    subscription

---
+++ app/src/main/java/tokyo/tommy_kw/musical/redux/core/Subscription.kt
@@ -0,0 +1,10 @@
+package tokyo.tommy_kw.musical.redux.core
+
+/**
+ * Created by tommy on 2016/06/27.
+ */
+class Subscription {
+    constructor(subscriber: StoreSubscriber, selector: Selector) {
+
+    }
+}
