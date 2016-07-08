619ba3117d096d20cfdaee4eff053e9a258bc080
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-07-08 21:03:05 +0900

    add layoutHandler

--- app/src/main/java/tokyo/tommy_kw/musical/adapter/BindingAdapter.kt
+++ app/src/main/java/tokyo/tommy_kw/musical/adapter/BindingAdapter.kt
@@ -2,6 +2,7 @@ package tokyo.tommy_kw.musical.adapter
 
 import android.content.DialogInterface
 import android.databinding.ViewDataBinding
+import android.support.annotation.LayoutRes
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -27,6 +28,10 @@ class BindingAdapter<T : Any>(): RecyclerView.Adapter<BindingAdapter.ViewHolder>
         private val map: MutableMap<Class<*>, Int> = mutableMapOf()
         private var onClick: View.OnClickListener? = null
         private var onLongClick: View.OnLongClickListener? = null
+
+        fun layoutHandler(layoutHandler: LayoutHandler) = apply {
+
+        }
     }
 
     class ViewHolder(val binding: ViewDataBinding,
@@ -40,5 +45,9 @@ class BindingAdapter<T : Any>(): RecyclerView.Adapter<BindingAdapter.ViewHolder>
                    ) {
 
         }
+    }
+
+    interface LayoutHandler {
+        @LayoutRes fun getItemLayout(item: Any, position: Int): Int
     }
 }
