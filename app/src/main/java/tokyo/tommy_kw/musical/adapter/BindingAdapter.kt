26fb3a25b62899984843c2c5a6491727cb46e46e
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-07-17 23:42:11 +0900

    add layoutHandler

--- app/src/main/java/tokyo/tommy_kw/musical/adapter/BindingAdapter.kt
+++ app/src/main/java/tokyo/tommy_kw/musical/adapter/BindingAdapter.kt
@@ -33,7 +33,8 @@ class BindingAdapter<T : Any>(): RecyclerView.Adapter<BindingAdapter.ViewHolder>
         private val map: MutableMap<Class<*>, Int> = mutableMapOf()
         private var onClick: View.OnClickListener? = null
         private var onLongClick: View.OnLongClickListener? = null
-        
+        private var handler: LayoutHandler? = null
+
         fun map(clazz: Class<*>, @LayoutRes layout: Int) = apply { map.put(clazz, layout) }
         inline fun <reified T: Any> map(@LayoutRes layout: Int) = map(T::class.java, layout)
 
