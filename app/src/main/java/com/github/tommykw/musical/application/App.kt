1d8337e70301d91e7ddca549d4ed29e11499ff64
tommykw <destinys.lotus.1108mn@gmail.com>
2020-06-10 06:48:51 +0900

    Add Hilt dependencies

--- app/src/main/java/com/github/tommykw/musical/application/App.kt
+++ app/src/main/java/com/github/tommykw/musical/application/App.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import com.facebook.stetho.BuildConfig
 import com.facebook.stetho.Stetho
 import com.firebase.client.Firebase
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 /**
- * Created by tommy on 15/10/07.
+ * Created by tommykw on 15/10/07.
  */
+@HiltAndroidApp
 class App : Application() {
     override fun onTerminate() {
         super.onTerminate()
